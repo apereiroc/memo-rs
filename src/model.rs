9c8a1f808870a72311281f55558f11fb02bdc971
Asier Pereiro <asier.pereiro.castro@cern.ch>
2024-05-11 23:56:43 +0200

    Update model and tests

--- src/model.rs
+++ src/model.rs
@@ -1,6 +1,3 @@
-use std::fs::File;
-use std::io::prelude::*;
-
 use crate::entry::EntryGroup;
 
 #[derive(Debug, Default, PartialEq, Eq)]
@@ -13,71 +10,27 @@ pub enum RunningState {
 
 #[derive(Debug, Default)]
 pub struct Model {
-    cache_path: String,
-    entries: Vec<EntryGroup>,
+    pub cache_path: String,
+    pub(crate) entries: Vec<EntryGroup>,
     pub running_state: RunningState,
 }
 
 impl Model {
-    pub fn new(cache_path: String, entries: Vec<EntryGroup>) -> Model {
+    pub fn new(cache_path: String) -> Model {
         Model {
             cache_path,
-            entries,
+            entries: vec![],
             running_state: RunningState::Empty,
         }
-    }
-
-    fn clear(&mut self) {
-        self.entries.clear();
-        self.running_state = RunningState::Empty;
-    }
-
-    fn load_from_cache(&mut self) {
-        if self.cache_path.is_empty() {
-            panic!("Trying to read previous status, but no path was specified");
-        }
-
-        // Read JSON data from the file
-        let mut file = File::open(&self.cache_path).expect("Failed to open cache file");
-        let mut json_data = String::new();
-        file.read_to_string(&mut json_data)
-            .expect("Failed to read cache file");
-
-        // Deserialize JSON data into a vector of Entry structs
-        let entry_groups: Vec<EntryGroup> =
-            serde_json::from_str(&json_data).expect("Deserialization failed");
-
-        // Print loaded entries for demonstration
-        for entry_group in entry_groups {
-            self.entries.push(entry_group);
-        }
-
-        self.running_state = RunningState::LoadedAndRunning;
-    }
-
-    fn save_to_cache(&mut self) {
-        if self.cache_path.is_empty() {
-            panic!("Trying to save current status, but no path was specified");
-        }
-
-        // Serialize EntryGroups to JSON
-        let json_data = serde_json::to_string(&self.entries).expect("Serialization failed");
-
-        let mut file = File::create(&self.cache_path).expect("Failed to create the cache file");
-
-        file.write_all(json_data.as_bytes())
-            .expect("Failed to write to file");
-
-        self.running_state = RunningState::SavedAndDone;
     }
 }
 
 #[cfg(test)]
-mod tests {
+pub mod tests {
     use super::*;
     use crate::entry::Entry;
 
-    fn make_test_entry_group() -> EntryGroup {
+    pub fn make_test_entry_group() -> EntryGroup {
         let entry1 = Entry {
             command: String::from("command1"),
             short_info: String::from("Short description 1"),
@@ -108,7 +61,8 @@ mod tests {
         let cache_path = String::from("test.cache");
         let entrygroup = make_test_entry_group();
 
-        let model = Model::new(cache_path, vec![entrygroup]);
+        let mut model = Model::new(cache_path);
+        model.entries = vec![entrygroup];
         assert_eq!(model.cache_path.len(), 10);
         assert_eq!(model.entries.len(), 1);
         assert_eq!(model.entries[0].entries.len(), 2);
@@ -120,11 +74,12 @@ mod tests {
     fn save_and_load_entries_to_cache() -> std::io::Result<()> {
         let entrygroup = make_test_entry_group();
 
-        let mut model = Model::new(String::from("test.cache"), vec![entrygroup]);
+        let mut model = Model::new(String::from("test.cache"));
+        model.entries = vec![entrygroup];
 
         model.save_to_cache();
 
-        model.clear();
+        model.entries.clear();
 
         model.load_from_cache();
 

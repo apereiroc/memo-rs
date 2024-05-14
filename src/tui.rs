cbd39eaf7fc8ceef4312632546a2ec994077275d
Asier Pereiro <asier.pereiro.castro@cern.ch>
2024-05-14 02:16:05 +0200

    Better panic errors

--- src/tui.rs
+++ src/tui.rs
@@ -18,11 +18,25 @@ pub fn restore_terminal() -> color_eyre::Result<()> {
     Ok(())
 }
 
-pub fn install_panic_hook() {
-    let original_hook = panic::take_hook();
+use color_eyre::{config::HookBuilder, eyre};
+pub fn install_panic_hook() -> color_eyre::Result<()> {
+    let (panic_hook, eyre_hook) = HookBuilder::default().into_hooks();
+
+    // convert from a color_eyre PanicHook to a standard panic hook
+    let panic_hook = panic_hook.into_panic_hook();
     panic::set_hook(Box::new(move |panic_info| {
-        stdout().execute(LeaveAlternateScreen).unwrap();
-        disable_raw_mode().unwrap();
-        original_hook(panic_info);
+        restore_terminal().unwrap();
+        panic_hook(panic_info);
     }));
+
+    // convert from a color_eyre EyreHook to a eyre ErrorHook
+    let eyre_hook = eyre_hook.into_eyre_hook();
+    eyre::set_hook(Box::new(
+        move |error: &(dyn std::error::Error + 'static)| {
+            restore_terminal().unwrap();
+            eyre_hook(error)
+        },
+    ))?;
+
+    Ok(())
 }

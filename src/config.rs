947790f5cb1c1c37481502d7546a5e07a6f0e61c
Asier Pereiro <asier.pereiro.castro@cern.ch>
2024-05-18 12:26:55 +0200

    Add keybindings

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,6 @@
 pub const PROJECT_NAME: &str = env!("CARGO_PKG_NAME");
 pub const PROJECT_VERSION: &str = env!("CARGO_PKG_VERSION");
 pub const PROJECT_AUTHOR: &str = env!("CARGO_PKG_AUTHORS");
+
+pub const KEYBINDINGS_INFO: &str =
+    " (q) quit | (↓) Go next | (↑) Go previous | (Enter) Go selected ";
